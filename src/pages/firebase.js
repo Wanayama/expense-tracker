ac7022bb411f30d0706bd5867cfba2b411803149
Brian Wanyama <brianwanyama06@gmail.com>
2025-02-12 00:31:16 +0300

    commit update

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,9 +1,11 @@
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVwGW7p2DNWkr9AfbdWnJDA0JskS-8y8I",
+  apiKey: 'FIREBASE_API_KEY',
   authDomain: "expense-tracker-933f2.firebaseapp.com",
   projectId: "expense-tracker-933f2",
   storageBucket: "expense-tracker-933f2.firebasestorage.app",
